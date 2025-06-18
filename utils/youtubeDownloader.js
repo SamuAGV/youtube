9e1a1dcc91ff304dfbdfa4a0f21a01ed57fb2ff9
Samuel <samuelagv06@gmail.com>
2025-06-17 22:52:28 -0600

    v2

--- utils/youtubeDownloader.js
+++ utils/youtubeDownloader.js
@@ -4,14 +4,24 @@ const path = require('path');
 
 const downloadVideo = async (url, quality = 'highest') => {
   try {
-    const info = await ytdl.getInfo(url);
+    // Añadir opciones de solicitud para evitar bloqueos
+    const options = {
+      quality: quality,
+      filter: format => format.container === 'mp4',
+      requestOptions: {
+        headers: {
+          // Headers para simular navegador
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
+          'Accept-Language': 'en-US,en;q=0.9'
+        }
+      }
+    };
+
+    const info = await ytdl.getInfo(url, options);
     const title = info.videoDetails.title.replace(/[^\w\s]/gi, '');
     const filePath = path.join(__dirname, '../../public', `${title}.mp4`);
-    
-    const video = ytdl(url, {
-      quality: quality,
-      filter: format => format.container === 'mp4'
-    });
+
+    const video = ytdl.downloadFromInfo(info, options);
     
     video.pipe(fs.createWriteStream(filePath));
     
